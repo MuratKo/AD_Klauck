a74d9de319ddc89a75fededf2500fa83e53fd527
sebdied <sebastian.diedrich@t-online.de>
2015-10-16 09:25:03 +0200

    Update final

--- Project/src/de/team/ast/Main.java
+++
@@ -1,9 +0,0 @@
-package de.team.ast;
-
-public class Main {
-
-	public static void main(String[] args){
-		ADTListe meineListe = ADTListe.create();
-		System.out.println(meineListe.laenge());
-	}
-}
