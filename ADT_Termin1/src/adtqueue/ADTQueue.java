ea991daf6bf5696f3f23d4e131609a573a6ac543
sebdied <sebastian.diedrich@t-online.de>
2015-10-26 08:48:40 +0100

    ADTQueue Version1

--- ADT_Termin1/src/adtqueue/ADTQueue.java
+++ ADT_Termin1/src/adtqueue/ADTQueue.java
@@ -1,5 +1,43 @@
 package adtqueue;
 
+import adtstack.ADTStack;
+
 public class ADTQueue {
+	
+	//Variablen
+	ADTStack stack1;
+	ADTStack stack2;
+	
+	//Konstruktor
+	private ADTQueue() {
+		
+	}
+	
+	//Operationen
+	
+	//create
+	public ADTQueue create() {
+		return new ADTQueue();
+	}
+	
+	//isEmpty
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	//enqueue
+	public ADTQueue enqueue(int elem) {
+		return new ADTQueue();
+	}
+	
+	//dequeue
+	public ADTQueue dequeue() {
+		return new ADTQueue();
+	}
+	
+	//front
+	public int front() {
+		return -1;
+	}
 
 }
