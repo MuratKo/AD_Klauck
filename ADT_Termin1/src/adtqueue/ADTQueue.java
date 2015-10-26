05d64700209824de28fd9b1fbf90db8c4cf59b9f
sebdied <sebastian.diedrich@t-online.de>
2015-10-26 12:17:44 +0100

    Udate ADTQueue

--- ADT_Termin1/src/adtqueue/ADTQueue.java
+++ ADT_Termin1/src/adtqueue/ADTQueue.java
@@ -5,39 +5,125 @@ import adtstack.ADTStack;
 public class ADTQueue {
 	
 	//Variablen
-	ADTStack stack1;
-	ADTStack stack2;
+	ADTStack stackIN;
+	ADTStack stackOUT;
 	
 	//Konstruktor
 	private ADTQueue() {
-		
+		stackIN = ADTStack.createS();
+		stackOUT = ADTStack.createS();
 	}
 	
 	//Operationen
 	
 	//create
-	public ADTQueue create() {
+	public static ADTQueue createQ() {
 		return new ADTQueue();
 	}
 	
+	
 	//isEmpty
-	public boolean isEmpty() {
+	public boolean isEmptyQ() {
+		if(this.getStackIN().isEmptyS() && this.getStackOUT().isEmptyS()) {
+			return true;
+		}
 		return false;
 	}
 	
-	//enqueue
-	public ADTQueue enqueue(int elem) {
-		return new ADTQueue();
+	
+	//enqueue (haengt ein Element an)
+	public ADTQueue enqueue(int element) {
+		//Copy Old Queue-Stacks
+		ADTStack copyIN = stackIN;
+		ADTStack copyOUT = stackOUT;
+		//Add element to stack 1
+		copyIN = copyIN.push(element);
+		ADTQueue returnQueue = ADTQueue.createQ();
+		returnQueue.setStackIN(copyIN);
+		returnQueue.setStackOUT(copyOUT);
+		return returnQueue;
+	}
+		//Fehlerbehandlung (es wurde kein int übergeben)
+		public ADTQueue enqueue(Object element){
+			return this;
+		}
+	
+	
+	//dequeue (entfernt das vorderste Element)
+	public ADTQueue dequeue() {
+		
+		//Fehlerbehandlung (Queue is totally empty)
+		if(this.isEmptyQ()) {
+			return this;
+		}
+		//Copy Old Queue-Stacks to return Queue
+		ADTQueue returnQueue = ADTQueue.createQ();
+		returnQueue.setStackIN(this.getStackIN());
+		returnQueue.setStackOUT(this.getStackOUT());
+
+		//Umstapeln, fall noetig):
+		if(this.stackOUT.isEmptyS()) {
+			returnQueue = umstapeln(this.getStackIN(), this.getStackOUT());
+		}
+		//vorderste Element entfernen
+		returnQueue.setStackOUT(returnQueue.getStackOUT().pop());
+		
+		return returnQueue;
 	}
 	
-	//dequeue
-	public ADTQueue dequeue() {
-		return new ADTQueue();
-	}
 	
 	//front
 	public int front() {
-		return -1;
+		
+		int returnValue;;
+		
+		//Fehlerbehandlung (Queue is totally empty)
+		if(this.isEmptyQ()) {
+			returnValue = -1;
+		}
+		else {
+			//Umstapeln, falls noetig):
+			if(this.getStackOUT().isEmptyS()) {
+				//Copy to stay functional
+				ADTQueue copy = umstapeln(this.getStackIN(), this.getStackOUT());
+				returnValue = copy.getStackOUT().top();
+			//Einfach aeltestes Element zurueck geben
+			} else {
+				returnValue = this.getStackOUT().top();
+			}
+		}
+		return returnValue;
 	}
+
+	
+		//Getter- und Setter-Methoden
+		private ADTStack getStackIN() {
+			return stackIN;
+		}
+	
+		private void setStackIN(ADTStack stackIN) {
+			this.stackIN = stackIN;
+		}
+	
+		private ADTStack getStackOUT() {
+			return stackOUT;
+		}
+	
+		private void setStackOUT(ADTStack stackOUT) {
+			this.stackOUT = stackOUT;
+		}
+		
+		//Umstapeln
+		private ADTQueue umstapeln(ADTStack stackINPUT, ADTStack stackOUTPUT) {
+			while(!stackINPUT.isEmptyS()) {
+				int tmpElement = stackINPUT.top();
+				stackOUTPUT = stackOUTPUT.push(tmpElement);
+				stackINPUT = stackINPUT.pop();
+			}
+		    ADTQueue umgestapelt = ADTQueue.createQ();
+		    umgestapelt.setStackIN(stackINPUT);
+		    umgestapelt.setStackOUT(stackOUTPUT);
+		    return umgestapelt;
+		}
 
 }
--- ADT_Termin1/src/main/Runner.java
+++ ADT_Termin1/src/main/Runner.java
@@ -1,9 +1,19 @@
 package main;
 
+import adtqueue.ADTQueue;
+import adtstack.ADTStack;
+
 public class Runner {
 
 	public static void main(String[] args) {
-		
+//		ADTQueue q1 = ADTQueue.createQ();
+//		q1 = q1.enqueue(15);
+//		q1 = q1.enqueue(16);
+//		q1 = q1.enqueue(17);
+//		q1 = q1.enqueue(18);
+//		q1 = q1.umstapeln(q1.getStackIN(),q1.getStackOUT());
+//		
+//		System.out.println(q1.getStackOUT().top());
 	}
 
 }
