b128a7c17742e2592fce1eeee1beb3eb4ab16790
sebdied <sebdied@gmx.net>
2015-10-28 08:25:50 +0100

    Update ADTQueueFunctionalTest2

--- ADT_Termin1/src/junitTest/ADTQueueFunctionalTest2.java
+++ ADT_Termin1/src/junitTest/ADTQueueFunctionalTest2.java
@@ -19,12 +19,39 @@ ADTQueue q1;
 	@Test
 	public void testIsEmpty() {
 		assertTrue(q1.isEmptyQ());
+		//100 Elemente einfuegen
+		for(int i=1; i<=200; i++) {
+			q1 = q1.enqueue(i);
+		}
+		assertFalse(q1.isEmptyQ());
+		//Vorderstes Element ist die 1
+		assertEquals(1, q1.front());
+		//Die ersten 90 Elemente enfernen
+		for(int i=1; i<=90; i++) {
+			q1 = q1.dequeue();
+		}
+		//Vorderstes Element ist jetzt die 91
+		assertEquals(91, q1.front());
 	}
 	
 	@Test
 	public void testIsEmpty2() {
 		q1 = q1.enqueue(1);
 		assertFalse(q1.isEmptyQ());;
+		//9999 Elemente zusaetzlich einfuegen => 10.000 Elemente
+		for(int i=2; i<=10000; i++) {
+			q1 = q1.enqueue(i);
+		}
+		assertFalse(q1.isEmptyQ());
+		//Vorderstes Element ist die 1
+		assertEquals(1, q1.front());
+		
+		//Die ersten 999 Elemente enfernen
+		for(int i=1; i<=999; i++) {
+			q1 = q1.dequeue();
+		}
+		//Vorderstes Element ist jetzt die 1000
+		assertEquals(1000, q1.front());
 	}
 	
 	@Test
@@ -45,6 +72,8 @@ ADTQueue q1;
 		assertEquals(1, q1.front());
 		q1 = q1.enqueue(2);
 		assertEquals(1, q1.front());
+		q1 = q1.enqueue(3);
+		assertEquals(1, q1.front());
 	}
 	
 	@Test
@@ -59,6 +88,9 @@ ADTQueue q1;
 	
 	@Test
 	public void testFront() {
+		//Fehlerbehandlung
+		assertEquals(-1,q1.front());
+		
 		q1 = q1.enqueue(1);
 		q1 = q1.enqueue(2);
 		q1 = q1.enqueue(3);
