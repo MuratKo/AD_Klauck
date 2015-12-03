efbdc84c4808c31c03989fe45f789d363bcf0caa
muratko <murat.korkmaz@haw-hamburg.de>
2015-12-04 00:31:04 +0100

    blabla

--- Termin3/programme/Source/src/Runner/Main.java
+++ Termin3/programme/Source/src/Runner/Main.java
@@ -1,6 +1,8 @@
 package Runner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 import ADTTree.ADTTree;
 import Helper.SortNum;
@@ -11,8 +13,9 @@ public class Main {
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
 		/*String path = "/home/murat/Dokumente/Uni/AD/Termin3/programme/zahlen/";
 		
@@ -37,34 +40,84 @@ public class Main {
 		
 		TreePNG.treeToPNG(pathOfFile + graphFileName, pathOfDestination + pngFileName);*/
 		
-		ADTTree tree = ADTTree.create(70);
+		ADTTree tree = ADTTree.create();
 		
-		tree.insert(60);
-		tree.insert(50);
-		tree.insert(40);
-		tree.insert(30);
-		tree.insert(20);
-		tree.insert(10);
-	
-		tree.insert(80);
-		tree.insert(90);
-		tree.insert(100);
-		tree.insert(120);
-		tree.insert(130);
-		tree.insert(140);
-		tree.insert(150);
-		tree.insert(160);
-		tree.insert(170);
-		tree.insert(180);
-		tree.insert(190);
+//		tree.insert(11);
+//		tree.insert(10);
+//		tree.insert(3);
+//		tree.insert(11);
+//		
+//		tree.insert(12);
+//		tree.insert(37);
+//		tree.insert(8);
+//		tree.insert(18);
+//		tree.insert(23);
+//		tree.insert(5);
+//		tree.insert(28);
+//		tree.insert(6);
+//		tree.insert(32);
+//		tree.insert(26);
+//		tree.insert(16);
+//		tree.insert(15);
+//		tree.insert(22);
+//		tree.insert(27);
+//		tree.insert(38);
+//		tree.insert(29);
+//		tree.insert(34);
+//		tree.insert(14);
+//		tree.insert(36);
+//		tree.insert(21);
+//		tree.insert(39);
+//		tree.insert(45);
+//		tree.insert(46);
+//		tree.insert(7);
+//		tree.insert(9);
+//		tree.insert(1);
+//		tree.insert(2);
+//		tree.insert(60);
+//		tree.insert(50);
+//		tree.insert(1);
+//		tree.insert(4);
 		
-		System.out.println(tree.getBiggerTreeHight());
-		System.out.println(tree.getSmallerTreeHight());
-		System.out.println(tree.getBiggerElement().getValue());
+		ArrayList<Integer> l = new ArrayList<>();
+		Random rand = new Random();
+		int anzahl = 200;
+		for(int i = 0 ; i < anzahl; i++){
+			
+			int n = rand.nextInt(anzahl) + 0;
+			if(!l.contains(n)){
+				
+				l.add(n);
+				
+			}
+		}
 		
+		for(int i : l){
+			tree.insert(i);
+		}
 		
-		tree.createPNG("/home/murat/Dokumente/Uni/AD/Termin3/programme/Graphen/GraphDateien/", "test.gv");
-		System.out.println(tree.getGraph());
+//		System.out.println(tree.high());
+//		System.out.println(tree.biggerElement.high());
+//		System.out.println(tree.smallerElement.high());
+//		System.out.println(tree.smallerElement.smallerElement.high());
+		
+//		System.out.println(tree.high());
+//		System.out.println(tree.getValue());
+//		
+//		System.out.println(tree.smallerElement.high());
+//		System.out.println(tree.smallerElement.getValue());
+//		
+//		System.out.println(tree.biggerElement.high());
+//		System.out.println(tree.biggerElement.getValue());
+//		
+//		System.out.println(tree.smallerElement.smallerElement.high());
+//		System.out.println(tree.smallerElement.smallerElement.getValue());
+//		
+//		System.out.println(tree.smallerElement.biggerElement.high());
+//		System.out.println(tree.smallerElement.biggerElement.getValue());
+		
+		tree.print("/home/murat/Dokumente/Uni/AD/Termin3/programme/Graphen/GraphDateien/", "m.gv");
+		
 	}
 
 
